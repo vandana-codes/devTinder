9c0e05caa0d2ca92880455e1f015cbfebb407cc3
vandana-codes <vandana.@Vandanas-MacBook-Air.local>
2025-05-21 23:21:04 +0530

    remove comment

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.use("/", (req, res) => {
-//     res.send("Hello vandana")
-
-// })
-
-
 app.use("/hello", (req, res) =>{
     res.send("Hello Hello Hello")
 })
